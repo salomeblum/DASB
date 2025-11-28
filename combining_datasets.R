d32c0e74c049d523db033490862fae9f7c2631c6
jogi96 <joris.gisler@stud.hslu.ch>
2025-11-28 10:26:40 +0100

    updatet joining

--- combining_datasets.R
+++ combining_datasets.R
@@ -1,54 +1,49 @@
-#Preprocessing Datasets
-# This Script is used to preprocess the Dataset so we can combine it at the end
+#Combining Dataset
+
 library(tidyverse)
-
-
-#load indivudial Datasets
-XWines_for_joining <- read.csv("data/Xwines_cleaned.csv", header = TRUE, sep = ",")
-XWinesratings_for_joining <- read.csv("data/XWines_avg_ratings.csv", header = TRUE, sep = ",")
-KaggleWines_for_joining <- read.csv("data/winemag.csv", header = TRUE , sep = ',')
-
-#Through inspection of the Dataset i found out that
-#we can match den winerys and the designation to the WineName and the WineryName
-
-library(dplyr)
 library(stringr)
 
+#Load Data
 
+XWines_for_joining <- read.csv("data/Xwines_cleaned.csv", sep = ",")
+winemag_for_joining <- read.csv("data/winemag.csv", sep = ",")
 
-KaggleWines_for_joining <- KaggleWines_for_joining %>%
+#rename Winemag 
+
+winemag_for_joining <- winemag_for_joining %>%
   rename(
     WineName = designation,
     WineryName = winery
   )
 
+#cleanup function strings for joining
 
-#Now we have to check if there are a lot of same values for X_Wines
-#this is for X_wines
-counter<- XWines_for_joining %>%
-  count(WineName) %>%
-  filter(n > 1)
+clean_string <- function(x) {
+  x %>%
+    str_to_lower() %>%            
+    str_replace_all("\\s+", "")   
+}
 
-#We can make a unique combination of Wine_Name and Winery_Name
-XWines_unique <- XWines_for_joining %>%
-  distinct(WineName, WineryName, .keep_all = TRUE)
+#cleaning strings
 
-KaggleWines_Unique <- KaggleWines_for_joining %>%
-  distinct(WineName, WineryName, .keep_all = TRUE)
+XWines_clean <- XWines_for_joining %>%
+  mutate(
+    WineName_clean = clean_string(WineName),
+    WineryName_clean = clean_string(WineryName)
+  )
 
+winemag_clean <- winemag_for_joining %>%
+  mutate(
+    WineName_clean = clean_string(WineName),
+    WineryName_clean = clean_string(WineryName)
+  )
 
+#innerjoin over cleaned WineName and WineryName
 
-#After that we can combine the Xwines and Kaggle Dataset with the WineName and the Winery
-joinedWines <- inner_join(XWines_unique,KaggleWines_Unique , by = c("WineName", "WineryName"))
+joinedWines <- inner_join(
+  XWines_clean,
+  winemag_clean,
+  by = c("WineName_clean", "WineryName_clean")
+)
 
-#we made the joins we now can join the Ratings
-completeDS <- inner_join(joinedWines, XWinesratings_for_joining, by = "WineID")
-
-#lets make a simple Plot to see the Ratings which are most common
-ggplot(completeDS, aes(x= avg_rating)) + geom_histogram(binwidth = 0.5, fill = "darkred", color = "white")+
-  labs(title = "Distribution of Ratings", x= "Rating", y="Count")
-
-#write it to an csv file
-write_csv(completeDS, "data/joined_dataset.csv")
-
-
+write.csv(joinedWines, "data/joined_dataset2.csv")
